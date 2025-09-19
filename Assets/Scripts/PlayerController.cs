268660f697eb45ba5530b89e20dd38cf990ba634
AJFirefox <AJFirefox08@gmail.com>
2025-09-19 10:46:28 -0500

    Jumping
    Need To Fix Jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,24 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float mouseSensitivity = 100f;
+    public float gravityValue = -3f;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    public float jumpHeight = 2.0f;
 
-    private CharacterController characterController;
+    private bool jumping;
+
+    private CharacterController Controller;
     private float xRotation = 0f;
 
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        Controller = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked; // Lock cursor to center of screen
     }
 
@@ -20,8 +28,33 @@ public class PlayerController : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
-        characterController.Move(move * moveSpeed * Time.deltaTime);
+        //groundedPlayer = Controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 2F; // Reset vertical velocity when grounded
+            jumping = false;
+            Debug.Log("DoneJumped");
+        }
+
+        // Handle Jump Input (using the new Input System example)
+        // If using the legacy Input System, replace with: if (Input.GetButtonDown("Jump") && groundedPlayer)
+        if (Input.GetButtonDown("Jump") && groundedPlayer && jumping == false)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
+            jumping = true;
+            groundedPlayer = false;
+            Debug.Log("Jumped");
+            //Controller.Move(playerVelocity * Time.deltaTime); // Move the character
+        }
+
+        
+
+        playerVelocity.y += gravityValue * Time.deltaTime; // Apply gravity
+        Controller.Move(playerVelocity * Time.deltaTime); // Move the character
+
+
+        Vector3 move = transform.right * x + transform.up * gravityValue + transform.forward * z;
+        Controller.Move(move * moveSpeed * Time.deltaTime);
 
         // Camera Look
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
@@ -32,5 +65,17 @@ public class PlayerController : MonoBehaviour
 
         Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         transform.Rotate(Vector3.up * mouseX);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+       // if (other.gameObject.layer == 10)
+            groundedPlayer = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //if (other.gameObject.layer == 10)
+            groundedPlayer = false;
     }
 }
